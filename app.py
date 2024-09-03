6fa2cb54bfa7721c65deed2cd9bced04fd3329aa
Vikash Innamuri <105922955+vikashinnamuri@users.noreply.github.com>
2024-09-03 10:54:19 +0530

    lsst

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def predictRoute():
 if __name__=="__main__":
     clApp=Clinetapp()
     #app.run(host='0.0.0.0',port=8080) # local
-    app.run(host='0.0.0.0',port=80) #azure
+    app.run(host='0.0.0.0',port=8080) #azure
     #app.run(host='0.0.0.0',port=8080)
             
         
